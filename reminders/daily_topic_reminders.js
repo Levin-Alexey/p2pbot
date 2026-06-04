9f42ae6be3538e169550f185c82a1a8ffcf72132
Levin-Alexey <levin.penza@yandex.ru>
2026-06-04 14:45:39 +0300

    fix

--- reminders/daily_topic_reminders.js
+++ reminders/daily_topic_reminders.js
@@ -1,7 +1,7 @@
 export const DAILY_TOPIC_REMINDERS = [
 	{
 		id: "update-links",
-		enabled: true,
+		enabled: false,
 		chatId: "-1003815117903",text: "⏰ НАПОМИНАНИЕ: обновите ссылки ПОКУПКУ/ПРОДАЖА в ответных сообщениях до 12:00 (МСК)\n\n🤖 Пришлите в двух ответных сообщении ссылку на 🟩 ПОКУПКУ USDT 🟩 и на 🟥 ПРОДАЖУ USDT 🟥\nBUY_LINK=...\n SELL_LINK=...",
 		messageThreadId: 4,
 		
