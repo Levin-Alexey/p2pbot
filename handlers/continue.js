ba9ba9917c8cf9fbcbc7bbf42fde124410d568a4
Levin-Alexey <levin.penza@yandex.ru>
2026-06-04 14:59:13 +0300

    fix

--- handlers/continue.js
+++ handlers/continue.js
@@ -29,11 +29,11 @@ export async function handleContinueCallback({ token, callbackQuery }) {
 			text: "Выберите действие:",
 			reply_markup: {
 				inline_keyboard: [
+					[{ text: "📊 КУРСЫ ОБМЕНА НА СЕГОДНЯ", callback_data: "today_exchange_rates" }],
 					[{ text: "💵 ПОКУПКА USDT", callback_data: "buy_usdt" }],
 					[{ text: "💰 ПРОДАЖА USDT", callback_data: "sell_usdt" }],
 					[{ text: "🏢 Заявка на обмен для юридических лиц (официально)", callback_data: "large_amount_request" }],
 					[{ text: "📨 Оставить заявку на крупную сумму", callback_data: "legal_exchange_request" }],
-					[{ text: "📊 Курсы обмена на сегодня", callback_data: "today_exchange_rates" }],
 					[{ text: "🌍 ВЭД-услуги (внешнеэкономическая деятельность)", callback_data: "ved_services" }],
 						[{ text: "🔒 Заморозка аккаунта биржи / Вывод средств после ВЭД", callback_data: "account_unfreeze_services" }],
 						[{ text: "🟢 Покупка BTC через OTC биржу (любая сумма, международные расчёты)", callback_data: "otc_services" }],
